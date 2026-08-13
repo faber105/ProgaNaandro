8c9ae80082b75416f05f9e1c4bf99a8ba34ee9bc
faber105 <akk2yrovnavad@gmail.com>
2026-08-13 16:29:28 +0200

    fix: complete Node polyfills and validate bundle before EAS

--- metro.config.js
+++ metro.config.js
@@ -5,6 +5,13 @@ const config = getDefaultConfig(__dirname);
 config.resolver.extraNodeModules = {
   stream: require.resolve('stream-browserify'),
   events: require.resolve('events'),
+  buffer: require.resolve('buffer'),
+  process: require.resolve('process/browser'),
+  util: require.resolve('util'),
 };
+
+config.serializer.getModulesRunBeforeMainModule = () => [
+  require.resolve('./polyfills.js'),
+];
 
 module.exports = config;
---
+++ polyfills.js
@@ -0,0 +1,2 @@
+global.Buffer = global.Buffer || require('buffer').Buffer;
+global.process = global.process || require('process');
