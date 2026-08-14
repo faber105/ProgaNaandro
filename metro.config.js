88e5d16655e9df36e18bc3b854e5ca02c3690be5
faber105 <akk2yrovnavad@gmail.com>
2026-08-14 14:29:06 +0200

    fix: force Metro core module aliases

--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,22 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-
-config.resolver.extraNodeModules = {
+const aliases = {
   stream: require.resolve('stream-browserify'),
   events: require.resolve('events'),
   buffer: require.resolve('buffer'),
   process: require.resolve('process/browser'),
   util: require.resolve('util'),
   zlib: require.resolve('browserify-zlib'),
+};
+
+config.resolver.extraNodeModules = aliases;
+const defaultResolveRequest = config.resolver.resolveRequest;
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (aliases[moduleName]) return { type: 'sourceFile', filePath: aliases[moduleName] };
+  return defaultResolveRequest
+    ? defaultResolveRequest(context, moduleName, platform)
+    : context.resolveRequest(context, moduleName, platform);
 };
 
 config.serializer.getModulesRunBeforeMainModule = () => [
